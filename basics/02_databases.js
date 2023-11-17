f3e4f29737727fccae403b2f5e53aefd4406891b
Rupesh Kumar Gupta <114826803+iRupesh02@users.noreply.github.com>
2023-11-17 09:33:57 +0000

    basics of Javascripts

---
+++ basics/02_databases.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+4) we are using Nodejs, not browser
+
+console.log(3+
+    3) // code readability should be high
+
+    console.log("rupesh");
+
+    let name="rupesh"
+    let age=18
+    let isLoggedIn=false
+    let state;
+    // number => 2 power 53
+    // bigint
+
+    //string =>""
+    //boolean => true/false
+    // null=>standalone value
+    //undefined=>
+
+    //symbol=> unique 
+
+    //object
+
+    console.log(typeof age);
+    console.log(typeof undefined);// undefined
+    console.log(typeof null);// object
